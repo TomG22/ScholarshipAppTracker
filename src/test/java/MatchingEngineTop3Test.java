9109b084f4a9b133623fd46e50210d3f902ded54
Samiur Rahman <srahman1@arizona.edu>
2025-12-01 02:54:50 -0700

    Create MatchingEngineTop3Test.java

---
+++ src/test/java/MatchingEngineTop3Test.java
@@ -0,0 +1,34 @@
+package backend;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MatchingEngineTop3Test {
+
+    public static void main(String[] args) {
+
+        MatchingEngine engine = new MatchingEngine();
+
+        Applicant a = new Applicant(
+                "Tester",
+                "Computer Science",
+                3.9,
+                Arrays.asList("coding", "ai", "research"),
+                Arrays.asList("leadership")
+        );
+
+        Scholarship s1 = new Scholarship("S1", 3.0, Arrays.asList("computer science"), Arrays.asList("coding"));
+        Scholarship s2 = new Scholarship("S2", 3.0, Arrays.asList("computer science"), Arrays.asList("ai"));
+        Scholarship s3 = new Scholarship("S3", 3.0, Arrays.asList("computer science"), Arrays.asList("research"));
+        Scholarship s4 = new Scholarship("S4", 3.0, Arrays.asList("computer science"), Arrays.asList("coding"));
+
+        List<Scholarship> all = Arrays.asList(s1, s2, s3, s4);
+
+        List<MatchResult> top3 = engine.topMatches(a, all, 3);
+
+        System.out.println("=== Top 3 Test ===");
+        for (MatchResult r : top3) {
+            System.out.println(r);
+        }
+    }
+}
